cc767fae17117ff7956f7167029c21d9f1fe57cc
Fici <mhurts00@estudiantes.unileon.es>
2025-02-18 00:53:18 +0100

    Ejercicio 7 Completo

--- Practica3/volumen.c
+++ Practica3/volumen.c
@@ -1,41 +1,42 @@
 #include <stdio.h>
 int main() {
 	int option;
-	double volumen;
+	float volumen, radio, altura, lado1, lado2;;
 	printf("Escribe el número de la figura de la que quieres calcular el volumen:\n1 - Cono\n2 - Ortoedro\n");
 	scanf("%d", &option);
 	switch (option){
 		case 1:
-			double radio, altura;
 			printf("Has seleccionado Cono (1)\n");
 			printf("Escribe su radio: \n");
-			scanf("%d", &radio);
+			scanf("%f", &radio);
 			printf("Escribe su altura: \n");
-			scanf("%d", &altura);
-			if (lado1 > 0 & lado2 > 0 & altura > 0) {
-				volumen = (1/3) * radio ^ (2) * altura;
-				printf("El volumen es: %.2d", &volumen);
+			scanf("%f", &altura);
+			if (radio > 0 && altura > 0) {
+				volumen = (1.0/3) * 3.1416 * radio * radio * altura;
+				printf("El volumen es: %.2f \n", volumen);
 			}
 			else {
-				printf("Error: No pueden usarse valores negativos.");
+				printf("Error: No pueden usarse valores negativos.\n");
 			}
+			break;
 		case 2:
-			double lado1, lado2, altura;
 			printf("Has seleccionado Ortoedro (2)\n");
 			printf("Escribe un lado: \n");
-			scanf("%d", &lado1);
+			scanf("%f", &lado1);
 			printf("Escribe otro lado: \n");
-			scanf("%d", &lado2);
+			scanf("%f", &lado2);
 			printf("Escribe su altura: \n");
-			scanf("%d", &altura);
-			if (lado1 > 0 & lado2 > 0 & altura > 0) {
-				volumen = (lado1 * lado2 * altura)
-				printf("El volumen es: %.2d", &volumen);
+			scanf("%f", &altura);
+			if (lado1 > 0 && lado2 > 0 && altura > 0) {
+				volumen = (lado1 * lado2 * altura);
+				printf("El volumen es: %.2f", volumen);
 			}
 			else {
-				printf("Error: No pueden usarse valores negativos.");
+				printf("Error: No pueden usarse valores negativos.\n");
 			}
+			break;
 		default:
-			printf("El valor %d no es una opción válida.", &option);
+			printf("El valor %.2d no es una opción válida.\n", option);
+	}
 	return 0;
 }
