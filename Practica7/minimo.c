d2678cac3d28652123e670e425b1d9f8b357e00f
MiguelHurtadoSantos <mhurts00@estudiantes.unileon.es>
2025-06-25 23:18:56 +0200

    Terminado minimo.c

--- Practica7/minimo.c
+++ Practica7/minimo.c
@@ -6,7 +6,8 @@
 void inicializamatriz(int **p, int n, int m);
 int calculaminimo(int **p, int n, int m);
 void main() {
-	int **p, n, m;
+	int n, m;
+	int **matriz;
 	srand(time(NULL));
 	printf("\n\nBienvenido!\nEscriba los datos que se le piden y el programa los leerá mostrando al final\nel menor de todos los números de la matriz generada.\n\n");
 	printf("Escriba un número de filas: ");
@@ -14,39 +15,42 @@ void main() {
 	printf("Escriba un número de columnas: ");
 	scanf("%d", &m);
 	
-	p = (int**) malloc (n * sizeof(int *));
+	matriz = (int**) malloc (n * sizeof(int *));
 	for(int i = 0; i < n; i++) {
-        p[i] = (int*) malloc (m * sizeof(int));
+        matriz[i] = (int*) malloc (m * sizeof(int));
     }
-	inicializamatriz(p, n, m);
+	inicializamatriz(matriz, n, m);
 	
 	printf("\n\nLa matriz aleatoria es:\n");
 	for (int i = 0; i < n; i++){
 	    for (int j = 0; j < n; j++){
-	        printf("%d   ", &p[i][j]);
+	        printf("%d\t", matriz[i][j]);
 	    }
 	    printf("\n");
 	}
 	
-	printf("\n\nValor mínimo: %d\n\n", calculaminimo(p, n, m));
+	printf("\n\nValor mínimo: %d\n\n", calculaminimo(matriz, n, m));
 	
-	free(p);
+	for(int i = 0; i < n; i++) {
+        free(matriz[i]);
+    }
+	free(matriz);
 }
 
-void inicializamatriz(int **p, int n, int m) {
+void inicializamatriz(int **matriz, int n, int m) {
     for(int i = 0 ; i < n ; i++) {
-        for(int j = 0; i < m ; j++) {
-            p[i][j] = rand() % 10 + 1;
+        for(int j = 0; j < m ; j++) {
+            matriz[i][j] = rand() % 10 + 1;
         }
     }
 }
 
-int calculaminimo(int **p, int n, int m) {
-	int min = p[0][0];
+int calculaminimo(int **matriz, int n, int m) {
+	int min = matriz[0][0];
 	for (int i = 0; i < n; i++) {
 	    for (int j = 0; j < m; j++) {
-	        if (p[i][j] < min) {
-	            min = p[i][j];
+	        if (matriz[i][j] < min) {
+	            min = matriz[i][j];
 	        }
 	    }
 	}
