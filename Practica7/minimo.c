480c7a8f5bef140e095d66b1499c6e37c066049c
MiguelHurtadoSantos <mhurts00@estudiantes.unileon.es>
2025-06-25 22:06:56 +0200

    1 error por solucionar

--- Practica7/minimo.c
+++ Practica7/minimo.c
@@ -1,34 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
+#include <time.h>
 
-int calculaminimo(int **p, int n);
+void inicializamatriz(int **p, int n, int m);
+int calculaminimo(int **p, int n, int m);
 void main() {
-	int nData, **p;
-	printf("\n\nBienvenido!\nEscriba números enteros y el programa los leerá mostrando al final\nel menor de todos los números proporcionados.\n\n");
-	printf("Primero escriba un número de datos que quiere introducir: ");
-	scanf("%d", &nData);
-	p = (int **) malloc (nData * sizeof(int *));
-	for(int i = 0; i < nData; i++) {
-		printf("\nEscriba un número: ");
-		scanf("%d", &**(p + i));
+	int **p, n, m;
+	srand(time(NULL));
+	printf("\n\nBienvenido!\nEscriba los datos que se le piden y el programa los leerá mostrando al final\nel menor de todos los números de la matriz generada.\n\n");
+	printf("Escriba un número de filas: ");
+	scanf("%d", &n);
+	printf("Escriba un número de columnas: ");
+	scanf("%d", &m);
+	
+	p = (int**) malloc (n * sizeof(int *));
+	for(int i = 0; i < n; i++) {
+        p[i] = (int*) malloc (m * sizeof(int));
+    }
+	inicializamatriz(p, n, m);
+	
+	printf("\n\nLa matriz aleatoria es:\n");
+	for (int i = 0; i < n; i++){
+	    for (int j = 0; j < n; j++){
+	        printf("%d   ", &p[i][j]);
+	    }
+	    printf("\n");
 	}
-	printf("\n\nLos valores introducidos son:");
-	for (int j = 0; j < nData; j++) {
-		printf("\nValor en posición %d: %d", j, **(p + j));
-	}
-	printf("\n\nValor mínimo: %d\n\n", calculaminimo(p, nData));
+	
+	printf("\n\nValor mínimo: %d\n\n", calculaminimo(p, n, m));
+	
+	free(p);
 }
 
-void inicializamatriz() {
+void inicializamatriz(int **p, int n, int m) {
+    for(int i = 0 ; i < n ; i++) {
+        for(int j = 0; i < m ; j++) {
+            p[i][j] = rand() % 10 + 1;
+        }
+    }
 }
 
-int calculaminimo(int **p, int n) {
-	int min = **(p + 0);
-	for (int i = 1; i < n; i++) {
-		if (**(p + i) < min) {
-			min = **(p + i);
-		}
+int calculaminimo(int **p, int n, int m) {
+	int min = p[0][0];
+	for (int i = 0; i < n; i++) {
+	    for (int j = 0; j < m; j++) {
+	        if (p[i][j] < min) {
+	            min = p[i][j];
+	        }
+	    }
 	}
 	return min;
 }
