6c2cf0e7a957a85db2a144b46f5e8242f4e9655d
MiguelHurtadoSantos <mhurts00@estudiantes.unileon.es>
2025-06-25 23:18:35 +0200

    Iniciado minimoFilas.c

---
+++ Practica7/minimoFilas.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <time.h>
+
+void minimoFilas(int **p, *v)
+
+int main() {
+  int **p, *v, n, m;
+  srand(time(NULL));
+  
+  v = (int*) malloc (n * sizeof(int));
+  p = (int**) malloc (n * sizeof(int*));
+  p[i] = (int*) malloc (m * sizeof(int));
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      p[i][j] = rand() % 21 - 10;
+    }
+  }
+
+  
+}
