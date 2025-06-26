8935ecc3f18ecd20c202dc38cbf9cc22c0dc5de8
MiguelHurtadoSantos <mhurts00@estudiantes.unileon.es>
2025-06-26 02:21:52 +0200

    Terminado minimoFilas.c

--- Practica7/minimoFilas.c
+++ Practica7/minimoFilas.c
@@ -3,21 +3,61 @@
 #include <malloc.h>
 #include <time.h>
 
-void minimoFilas(int **p, *v)
+void minimoFilas(int **matriz, int *vector, int n, int m);
 
 int main() {
-  int **p, *v, n, m;
+  int **matriz;
+  int *vector;
+  int n, m;
   srand(time(NULL));
   
-  v = (int*) malloc (n * sizeof(int));
-  p = (int**) malloc (n * sizeof(int*));
-  p[i] = (int*) malloc (m * sizeof(int));
+  printf("\nEscriba número de filas: ");
+  scanf("%d", &n);
+  printf("\nEscriba número de columnas: ");
+  scanf("%d", &m);
+  
+  vector = (int*) malloc (n * sizeof(int));
+  matriz = (int**) malloc (n * sizeof(int*));
+  for (int i = 0; i < n; i++) {
+      matriz[i] = (int*) malloc (m * sizeof(int));
+  }
 
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < m; j++) {
-      p[i][j] = rand() % 21 - 10;
+      matriz[i][j] = rand() % 21 - 10;
     }
   }
 
+  minimoFilas(matriz, vector, n, m);
+  //Imprimir vector
+    
+    for (int i = 0; i < n; i++){
+	    for (int j = 0; j < m; j++){
+	        printf("%d\t", matriz[i][j]);
+	    }
+	    printf("\n");
+	}
+	printf("\n");
+    for(int i = 0; i < n; i++) {
+        printf("%d\t", vector[i]);
+    }
+  //Liberar vector y matriz
   
+    for(int i = 0; i < n; i++) {
+        free(matriz[i]);
+    }
+    free(matriz);
+    free(vector);
+}
+
+void minimoFilas(int **matriz, int *vector, int n, int m) {
+	for (int i = 0; i < n; i++) {
+	    int min = matriz[i][0];
+	    for (int j = 0; j < m; j++) {
+	        if (matriz[i][j] < min) {
+	            min = matriz[i][j];
+	        }
+	    }
+	    vector[i] = min;
+	}
 }
